f381828f0838de62760bd9bf4ab52cc206f651b5
patr4586 <patr4586@ZBC-E-RO-23234>
2021-11-26 10:28:43 +0100

    commit

--- App.js
+++ App.js
@@ -8,5 +8,30 @@ function CalculateBMI(weight, height)
 
 function OnClick()
 {
-    document.getElementById("bmi").innerHTML = CalculateBMI(parseInt(Weight.value), parseInt(Height.value));
+    let bmi = CalculateBMI(parseInt(Weight.value), parseInt(Height.value));
+
+    if (bmi >= 16 && bmi <= 18.5)
+    {
+        document.getElementById("bmi").innerHTML = "You're underweight";
+    }
+    else if (bmi >= 18.5 && bmi <= 24)
+    {
+        document.getElementById("bmi").innerHTML = "You're normal";
+    }
+    else if (bmi >= 24 && bmi <= 30)
+    {
+        document.getElementById("bmi").innerHTML = "You're fat";
+    }
+    else if (bmi >= 30 && bmi <= 35)
+    {
+        document.getElementById("bmi").innerHTML = "You're very fat";
+    }
+    else if (bmi >= 35 && bmi <= 40)
+    {
+        document.getElementById("bmi").innerHTML = "You're insanely fat";
+    }
+    else if (bmi > 40)
+    {
+        document.getElementById("bmi").innerHTML = "You have your own gravity";
+    }
 }
